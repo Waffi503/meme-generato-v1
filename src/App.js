69f5c2a7a7715d0fb43e8ab75abade03c67d5996
Waffi503 <carlosbrandan503@gmail.com>
2022-01-02 13:27:33 -0300

    primer y unico commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react';
+import html2canvas from 'html2canvas';
+import "./App.css";
 function App() {
+
+  const [linea1, setLinea1] = useState("Linea 1");
+  const [linea2, setLinea2] = useState("Linea 2");
+  const [img, setImg] = useState("1meme.jfif");
+
+  const exportarMeme = () => {
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      const imgData = canvas.toDataURL("image/png");
+      const link = document.createElement("a");
+      link.href = imgData;
+      link.download = "meme.png";
+      link.click();
+
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <select onChange={(e)=> setImg(e.target.value)}>
+        <option value="1meme.jfif">Meme 1</option>
+        <option value="2meme.jpg">Meme 2</option>
+        <option value="3meme.jpg">Meme 3</option>
+        <option value="4meme.jfif">Meme 4</option>
+      </select>
+      <br/>
+     
+      <input type="text" placeholder="Segunda linea" onChange={(e)=> setLinea1(e.target.value)}/>
+      <br/>
+      <input type="text" placeholder="Primer linea" onChange={(e)=> setLinea2(e.target.value)} />
+      <br/>
+      <button onClick={exportarMeme}>Crear meme</button>
+      <div className='meme' id='meme'>
+        <span className='linea1'>{linea1}</span>
+        <br/>
+        <span className='linea2'>{linea2}</span>
+        <img className='img_meme' src={`imgs/${img}`} alt="meme"/>
+      </div>
+     
+      
+
+     
+      
     </div>
   );
 }
